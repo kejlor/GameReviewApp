c382dd36beabc7d3256f31bfde79e1539f4592f5
kejlor <woj450@gmail.com>
2022-08-08 12:51:57 +0200

    Initial Commit

---
+++ GameReviewApp/GameReviewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameReviewAppApp.swift
+//  GameReviewApp
+//
+//  Created by Bartosz Wojtkowiak on 08/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct GameReviewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
